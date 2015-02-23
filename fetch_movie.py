0d74cbb6b95c8d2ac13911d08b32756d682aae36
Wei Chen <wei.chen.riceu@gmail.com>
2015-02-23 15:47:00 -0800

    add fetch_movie get movie according year

--- fetch_movie.py
+++ fetch_movie.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup as BS
 
 IMDbURL = "http://www.imdb.com"
 
+def end_flag(text):
+    total = text.split()
+    return total[0].split('-')[1] != total[2]
+
 def get_movie_list(year, start=1, count=100, country='us', ttype='feature'):
     url = IMDbURL + '/search/title?' + \
-          'country=' + country + '&' + \
+          'countries=' + country + '&' + \
           'release_date=' + str(year) + '&' + \
           'title_type=' + ttype + '&' + \
           'start=' + str(start) + '&' + \
@@ -13,6 +17,19 @@ def get_movie_list(year, start=1, count=100, country='us', ttype='feature'):
           'count=' + str(count)
     r = requests.get(url)
     soup = BS(r.content, 'html.parser')
+    flag = end_flag(soup.find('div', id='left').text)
+    
+
     return soup
+
+
+
+
+def get_movie_list_per_year(year, country='us', ttype='feature'):
+    i = 0
+    count = 100
+    while nleft <
+    get_movie_list(year=year, )
+
 
 
